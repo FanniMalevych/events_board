11cadcb8096752c3a9431074d347bef199de626c
fanni <fmalevych@zoolatech.com>
2024-05-13 16:20:46 +0300

    minor fix

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:3000',
-        target: 'https://events-board-api.vercel.app/',
+        target: 'https://events-board-api.vercel.app',
         changeOrigin: true
       }
     }
